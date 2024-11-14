1cb1a63ed6b8945f57915f34685ccd7319f5fd2b
Val dawson <vdawson647@gmail.com>
2024-11-14 03:58:39 -0500

    More style changes to the resume

--- components/Resume/Resume.jsx
+++ components/Resume/Resume.jsx
@@ -17,7 +17,7 @@ const Resume = () => {
                 <div className='resumeHeader'>
                     <h1>Valmon Dawson</h1>
                     <h2>Front-End Developer</h2>
-                    <h3>347.605.3414</h3>
+                    {/* <h3>347.605.3414</h3> */}
                     <h3><a href="mailto:VDawson647@gmail.com">VDawson647@gmail.com</a></h3>
                     <h3><a href="https://www.linkedin.com/in/vdawson647/"  target="_blank">https://www.linkedin.com/in/vdawson647/</a></h3>
                     <h3>https://vdawson647.dev/</h3>
@@ -54,7 +54,7 @@ const Resume = () => {
                             <p className='employerHeader'>Publishers Clearing House</p>
                             <ul>
                                 <li className='employerJob'>
-                                    <p className='subHeader'>Senior Front-End Developer <span>Apr. 2017 - Oct. 2024 — Jericho, NY</span></p>
+                                    <p className='subHeader'>Senior Front-End Developer - <span>Apr. 2017 - Oct. 2024 — Jericho, NY</span></p>
                                     <ul className='responsibilityList'>
                                         <li>Created components for PCH's rewards site.</li>
                                         <li>Built eCommerce landing pages.</li>
@@ -68,11 +68,26 @@ const Resume = () => {
                                     </ul>
                                 </li>
                                 <li className='employerJob'>
-                                    <p className='subHeader'>Front-End Developer <span>Apr. 2009 - Apr. 2017 — Port Washington, NY</span></p>
+                                    <p className='subHeader'>Front-End Developer - <span>Apr. 2009 - Apr. 2017 — Port Washington, NY</span></p>
                                     <ul className='responsibilityList'>
                                         <li>Develop, modify, troubleshoot, and maintain bespoke eCommerce landing pages (packages) created with HTML5, CSS3, JavaScript, & GSAP.</li>
                                         <li>Modified, troubleshot, and maintained the continuing useability of our eCommerce landing page library.</li>
                                         <li>Work closely with the creative, legal, marketing, and operations teams to create, update, fix, and maintain our expanding library of eCommerce landing pages.</li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <p className='employerHeader'>Venture Direct</p>
+                            <ul>
+                                <li className='employerJob'>
+                                    <p className='subHeader'>Web Producer / QA Specialist - <span>Sept. 2006 - Jan. 2009 — New York, NY</span></p>
+                                    <ul className='responsibilityList'>
+                                        <li>Created, Updated, and Maintained lead-generation microsites for online schools based on client specifications using a combination of HTML/CSS/Javascript/JQuery/PHP.</li>
+                                        <li>Verified the integrity of leads that were generated.</li>
+                                        <li>Troubleshooting, and resolving HTTP Post issues</li>
+                                        <li>Talked with clients and worked with cross functional team to resolve lead generation issues.</li>
+                                        <li>Providing training & guidance to new team members.</li>
                                     </ul>
                                 </li>
                             </ul>
@@ -85,21 +100,20 @@ const Resume = () => {
                     <ul className="sectionMain">
                         <li>
                             <p className='subHeader'>New York City College Of Technology</p>
-                            <p className="subCopy">Bachelor's of Technology <span>Sept. 2004 - Aug 2006,  Brooklyn, NY</span></p>
+                            <p className="subCopy">Bachelor's of Technology - <span>Brooklyn, NY</span></p>
                             <ul className='responsibilityList'>
                                 <li>Major: Computer Systems - GPA: 3.5</li>
                                 <li>Areas of Concentration: Web Design and Implementation, Program Design, LAN</li>
                                 <li>Accomplishments: Magna Cum Laude; Dean's List - Spring 2005 and Spring 2006</li>
                             </ul>
                         </li>
-                        <li>
+                        {/* <li>
                             <p className='subHeader'>Borough of Manhattan Community College</p>
                             <p className="subCopy">Associate's Degree <span>Jan. 2002 - Jun. 2004,  New York, NY</span></p>
                             <ul className='responsibilityList'>
                                 <li>Major: Computer Programming</li>
                             </ul>
-                        </li>
-
+                        </li> */}
                     </ul>
                 </div>
             </div>
